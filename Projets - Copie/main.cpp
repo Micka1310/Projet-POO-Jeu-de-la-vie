99630d4ec5f0fa04166f2ec46f6c1e852f7cf6c6
billy951 <123874985+billy951@users.noreply.github.com>
2024-11-25 00:19:34 +0100

    Add files via upload

--- Projets - Copie/main.cpp
+++ Projets - Copie/main.cpp
@@ -1,21 +1,15 @@
-#include <iostream>
-#include "Grille.h"
+#include "generation.h"
 
 int main() {
-    int choix = 1;
-    Grille maGrille;
-    maGrille.getTaille();
-    maGrille.afficherGrille();
-    while (choix == 1){
-        cout << "Voulez placer une case? :\n";
-        cout << "1. Oui\n";
-        cout << "0. Non\n";
-        cout << "Votre choix : ";
-        cin >> choix;
-        if (choix == 1){
-            maGrille.PlacerPoint();
-            maGrille.afficherGrille();
-        }
+    // Initialisation avec un maximum de 10 générations
+    Generation gen(10);
+
+    while (!gen.estTerminee()) {
+        gen.afficherGeneration();
+        gen.incrementer();
     }
+
+    std::cout << "Le processus est termine." << std::endl;
+
     return 0;
 }
