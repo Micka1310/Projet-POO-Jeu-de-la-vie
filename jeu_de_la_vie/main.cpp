dfc8bac3ebecce768a976a409a9c4fc54cdc213a
mickaël King <mickaelking13@gmail.com>
2024-12-03 22:36:02 +0100

    ajout du fichier 'main.cpp'

---
+++ jeu_de_la_vie/main.cpp
@@ -0,0 +1,16 @@
+// =======================
+// | Programme principal |
+// =======================
+//
+// main.cpp : Ce fichier permet de démarrer le programme principal.
+// 
+// Contient la fonction "main()".
+
+#include "case.h"
+
+int main() 
+{   
+    std::cout << "Le programme principal marche ! :)" << std::endl;
+
+    return 0;
+}
