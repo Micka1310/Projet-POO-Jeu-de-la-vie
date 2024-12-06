b36d8f2c51a50e953418b4f4a8f388e479c172f5
billy951 <123874985+billy951@users.noreply.github.com>
2024-12-06 12:10:00 +0100

    Mise à jour generation (#11)
    * creation dépot
    
    * Ma troisieme sauvergarde
    
    * ma toisieme sauvergarde
    
    * J'ai ajouté generation.cpp
    
    * Ajout de test unitaire pour generation
    
    ---------
    
    Co-authored-by: Bilel Rouabah <bilel.rouabah@viacesi.fr>

--- jeu_de_la_vie/Generation.h
+++ jeu_de_la_vie/Generation.h
@@ -1,34 +1,23 @@
 #pragma once
 #include <iostream>
+
 class Generation {
 private:
     int generation;  // Num de la generation
-    int max_gen;     // Nombre max de générations
+    int max_gen;     // Nombre max de g�n�rations
+
 public:
     // Constructeur
-    Generation(int maxGenerations) : generation(0), max_gen(maxGenerations) {}
-    // Méthode incrémenter génération
-    void incrementer() {
-        if (generation < max_gen) {
-            generation++;
-        }
-    }
-    // Méthode pour comparer la génération avec max
-    bool estTerminee() const {
-        return generation >= max_gen;
-    }
-    int getGeneration() const {
-        return generation;
-    }
-    int getMaxGeneration() const {
-        return max_gen;
-    }
-    void arreter() {
-        generation = max_gen;
-    }
-    // Affichage des informations de la génération
-    void afficherGeneration() const {
-        std::cout << "Generation actuelle : " << generation+1
-                  << " / Nombre maximum de generations : " << max_gen << std::endl;
-    }
+    Generation(int maxGenerations);
+
+    // Destructeur
+    ~Generation();
+
+    // M�thodes
+    void incrementer();
+    bool estTerminee() const;
+    int getGeneration() const;
+    int getMaxGeneration() const;
+    void arreter();
+    void afficherGeneration() const;
 };
