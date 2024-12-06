86cd6c5601dda1e758dc6420a3c3dc5d92120bf5
Bilel Rouabah <bilel.rouabah@viacesi.fr>
2024-12-06 11:58:05 +0100

    J'ai ajouté generation.cpp

--- jeu_de_la_vie/Generation.h
+++ jeu_de_la_vie/Generation.h
@@ -1,39 +1,23 @@
 #pragma once
 #include <iostream>
+
 class Generation {
 private:
     int generation;  // Num de la generation
-    int max_gen;     // Nombre max de générations
+    int max_gen;     // Nombre max de g�n�rations
+
 public:
     // Constructeur
-    Generation(int maxGenerations) : generation(0), max_gen(maxGenerations) {}
+    Generation(int maxGenerations);
 
-    // Destructeur 
-    ~Generation() {
-        std::cout << "Destructeur : L'objet Generation est detruit" << std::endl;
-    }
-    // Méthode incrémenter génération
-    void incrementer() {
-        if (generation < max_gen) {
-            generation++;
-        }
-    }
-    // Méthode pour comparer la génération avec max
-    bool estTerminee() const {
-        return generation >= max_gen;
-    }
-    int getGeneration() const {
-        return generation;
-    }
-    int getMaxGeneration() const {
-        return max_gen;
-    }
-    void arreter() {
-        generation = max_gen;
-    }
-    // Affichage des informations de la génération
-    void afficherGeneration() const {
-        std::cout << "Generation actuelle : " << generation+1
-                  << " / Nombre maximum de generations : " << max_gen << std::endl;
-    }
+    // Destructeur
+    ~Generation();
+
+    // M�thodes
+    void incrementer();
+    bool estTerminee() const;
+    int getGeneration() const;
+    int getMaxGeneration() const;
+    void arreter();
+    void afficherGeneration() const;
 };
